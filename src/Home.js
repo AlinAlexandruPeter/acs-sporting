b11dc7f59b4cbb3f4d31ca0fa5f79356427a184c
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-04-19 11:24:52 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,21 @@
 import React from 'react';
-import { useState } from 'react'
-
-import Home from './Home';
-import Team from './Team';
-import TeamU9 from './TeamU9';
-import TeamU13 from './TeamU13';
-import Redirect from "./Redirect";
-import Partners from './Partners';
-import Contact from './Contact';
-import Program from './Program';
-import Gallery from './Gallery';
-import PersonalTraining from './PersonalTraining';
 
 import Navbar from './components/Navbar';
 import NavPhones from "./components/NavPhones";
 import Menu from './components/Menu';
+import Hero from './components/Hero';
+import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("home");
-
-  const handlePageChange = selectedPage => {
-    setCurrentPage(selectedPage)
-  }
 	
   return (
-    <div className="app">
+    <div>
         <Navbar />
         <NavPhones />
         <Menu />
-        {currentPage === "home" && <Home />}
-        {currentPage === "partners" && <Partners />}
-        {currentPage === "contact" && <Contact />}
-        {currentPage === "echipe" && <Team />}
-        {currentPage === "u9" && <TeamU9 />}
-        {currentPage === "u13" && <TeamU13 />}
-        {currentPage === "redirect" && <Redirect />}
-        {currentPage === "program" && <Program />}
-        {currentPage === "galerie" && <Gallery />}
-        {currentPage === "antrenament" && <PersonalTraining />}
+	  <Hero />
+	  <AboutUs />
         <Footer />
     </div>
   );
