e4e61acf187af026df9cc7e0b741a4176d296820
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-04-19 11:21:54 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react'
 
-import "./index.scss";
-import "./css/h-f-phones.scss";
+import Home from './Home';
+import Team from './Team';
+import TeamU9 from './TeamU9';
+import TeamU13 from './TeamU13';
+import Redirect from "./Redirect";
+import Partners from './Partners';
+import Contact from './Contact';
+import Program from './Program';
+import Gallery from './Gallery';
+import PersonalTraining from './PersonalTraining';
 
 import Navbar from './components/Navbar';
 import NavPhones from "./components/NavPhones";
 import Menu from './components/Menu';
-import Hero from './components/Hero';
-import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 
-import playersU9 from "./js/playersU9";
-import playersU13 from "./js/playersU13";
+function App() {
+  const [currentPage, setCurrentPage] = useState("home");
 
-export default function Home() {
+  const handlePageChange = selectedPage => {
+    setCurrentPage(selectedPage)
+  }
 	
-  const allPlayers = [...playersU9, ...playersU13];
-	
-	return (
-		<div>
-			<Navbar />
-			<NavPhones />
-			<Menu />
-			<Hero />
-			<AboutUs />
-			<Footer />
-    	</div>
-	)
+  return (
+    <div className="app">
+        <Navbar />
+        <NavPhones />
+        <Menu />
+        {currentPage === "home" && <Home />}
+        {currentPage === "partners" && <Partners />}
+        {currentPage === "contact" && <Contact />}
+        {currentPage === "echipe" && <Team />}
+        {currentPage === "u9" && <TeamU9 />}
+        {currentPage === "u13" && <TeamU13 />}
+        {currentPage === "redirect" && <Redirect />}
+        {currentPage === "program" && <Program />}
+        {currentPage === "galerie" && <Gallery />}
+        {currentPage === "antrenament" && <PersonalTraining />}
+        <Footer />
+    </div>
+  );
 }
+
+export default App;
