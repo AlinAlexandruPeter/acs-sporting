0223b0cd6a5177c3356554c7ae9587024c2881f1
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2023-04-19 11:29:12 +0300

    Create Model.js

---
+++ src/Model.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Hero from './components/Hero';
+import MainOfPage from './components/AboutUs';
+
+function Model() {
+	
+  return (
+    <div>
+        <Hero />
+        <MainOfPage />
+    </div>
+  );
+}
+
+export default Model;
